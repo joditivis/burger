1901184bc54e18599f591ca27cf0eb3df20477cb
Jodi Tivis <joditivis@gmail.com>
2020-01-14 21:52:17 -0700

    changed table content within seeds.sql file. Added burgers.js file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,1 @@
-INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
-INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Meat Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
