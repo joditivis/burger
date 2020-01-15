4e7f8acaaf1978c208af821208d64843990443b0
Jodi Tivis <joditivis@gmail.com>
2020-01-14 21:29:18 -0700

    changed controllers file back to first direction I was trying

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,8 +6,8 @@ var router = express.Router();
 var burger = require('../models/burger.js');
 
 // create all routes and set up logic within those routes where required
-router.get('/', function(req, res) {
-    burger.selectAll(function(data) {
+router.get('/', function (req, res) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
@@ -16,28 +16,36 @@ router.get('/', function(req, res) {
     });
 });
 
-router.post('/api/burgers', function(req, res) {
+router.post('/api/burgers', function (req, res) {
     burger.insertOne([
-        'burger_name'
+        'burger_name', 'devoured'
     ], [
-        req.body.burger_name
-    ], function(result) {
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
         // send back the ID of the new burger
         res.json({ id: result.insertId });
         res.redirect('/');
     });
 });
 
-router.put('api/burgers/:id', function(req, res) {
+router.put('api/burgers/:id', function (req, res) {
     var condition = 'id = ' + req.params.id;
 
     console.log('condition', condition);
 
-    burger.updateOne({
-        devoured: true
-    }, condition, function(result) {
-        res.redirect('/');
-    });
+    burger.updateOne(
+        {
+            devoured: req.body.devoured
+        },
+        condition, function (result) {
+            if (result.changedRows == 0) {
+                // if no rows were changed then the ID doesn't exist, so return status 404
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+            res.redirect('/');
+        });
 });
 
 // export routes for server.js to use
